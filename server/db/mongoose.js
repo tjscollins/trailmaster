061441c110f71d7f601e016de0e5e69e6936713e
Tyler Collins <tjscollins@gmail.com>
2016-12-11 20:09:02 +1000

    Tweak for Heroku Deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TrailMaster');
-mongoose.connect('mongodb://heroku_g6w3h74t:vmbrf4mj23ocpikbgun6id2j3k@ds127998.mlab.com:27998/heroku_g6w3h74t');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TrailMaster');
 
 module.exports = {
   mongoose
