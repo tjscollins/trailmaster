56672ef956141479737981c43e0b8bca10607be1
Tyler Collins <tjscollins@gmail.com>
2016-12-27 09:27:18 +1000

    Bugfix: Move map to user pos even with auto-center disabled

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var webpack = require('webpack');
-// var path = require('path');
 
 module.exports = {
   entry: [
@@ -65,21 +64,6 @@ module.exports = {
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
-        // }, {
-        //   test: /\.css$/,
-        //   loader: 'style-loader!css-loader'
-        // }, {
-        //   test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        //   loader: 'file'
-        // }, {
-        //   test: /\.(woff|woff2)$/,
-        //   loader: 'url?prefix=font/&limit=5000'
-        // }, {
-        //   test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        //   loader: 'url?limit=10000&mimetype=application/octet-stream'
-        // }, {
-        //   test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        //   loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
     ]
   },
