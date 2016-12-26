4fd3d8e9c0a33b501900a3d700b1e43964fdbb57
Tyler Collins <tjscollins@gmail.com>
2016-12-26 11:59:00 +1000

    Cleaned up UpdatePOI interface and prepped for heroku testing

--- webpack.config.js
+++ webpack.config.js
@@ -10,21 +10,21 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({'$': 'jquery', 'jQuery': 'jquery', 'jquery': 'jquery'}),
-    // new webpack
-    //   .optimize
-    //   .OccurenceOrderPlugin(),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // }),
-    // new webpack
-    //   .optimize
-    //   .UglifyJsPlugin({
-    //     compressor: {
-    //       warnings: false
-    //     }
-    //   })
+    new webpack
+      .optimize
+      .OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack
+      .optimize
+      .UglifyJsPlugin({
+        compressor: {
+          warnings: false
+        }
+      })
   ],
   output: {
     path: __dirname,
@@ -83,5 +83,5 @@ module.exports = {
       }
     ]
   },
-  devtool: 'eval-source-map'
+  // devtool: 'eval-source-map'
 };
