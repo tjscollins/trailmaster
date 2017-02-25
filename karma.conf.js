a19b1a1ec082837132268d4eb21a10e7b64d9906
Tyler Collins <tjscollins@gmail.com>
2017-02-25 13:31:29 +1000

    Incorporated istanbul for code coverage reports of unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -3,31 +3,51 @@ const webpackConfig = require('./webpack.config.dev.js');
 module.exports = function(config) {
   config.set({
     autowatch: false,
-    browsers: ['PhantomJS'],
-    singleRun: true,
-    frameworks: [
-      'mocha', 'sinon'
+    browsers: [
+      'PhantomJS',
     ],
-    files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.min.js',
-      'tests/client/**/*.test.jsx'
-    ],
-    preprocessors: {
-      'tests/client/**/*.test.jsx': ['webpack', 'sourcemap']
-    },
-    reporters: ['mocha'],
+    browserNoActivityTimeout: 100000,
     client: {
       captureConsole: true,
       mocha: {
         bail: true,
-        timeout: '5000'
-      }
+        timeout: '5000',
+      },
     },
-    browserNoActivityTimeout: 100000,
+    coverageReporter: {
+      reporters: [
+        {
+          type: 'html',
+          dir: 'coverage/',
+        }, {
+          type: 'text-summary',
+        },
+      ],
+    },
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'tests/client/**/*.test.js',
+      'tests/client/**/*.test.jsx',
+    ],
+    frameworks: [
+      'mocha', 'sinon',
+    ],
+    preprocessors: {
+      'tests/client/**/*.test.js': [
+        'webpack', 'sourcemap', 'coverage',
+      ],
+      'tests/client/**/*.test.jsx': [
+        'webpack', 'sourcemap', 'coverage',
+      ],
+    },
+    reporters: [
+      'mocha', 'coverage',
+    ],
+    singleRun: true,
     webpack: webpackConfig,
     webpackServer: {
-      noInfo: true
-    }
+      noInfo: true,
+    },
   });
 };
