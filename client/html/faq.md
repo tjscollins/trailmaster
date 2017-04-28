8aaa6426835ddb69260ef5d5a43e6c4d5efdcd37
Tyler Collins <tjscollins@gmail.com>
2017-04-28 13:57:36 +1000

    Refactored FAQ.jsx to use Markdown for its content

---
+++ client/html/faq.md
@@ -0,0 +1,29 @@
+1.  **What is Trailmaster?**
+
+   Trailmaster is a publicly accessible, open-source web application for storing, sharing, and using GPS data from wearable athletic devices to plan new trail running and mountain biking courses.
+
+2.  **How do I use Trailmaster?**
+
+   The website is meant to be simple to use.  [This Youtube video](https://www.youtube.com/watch?v=spYawo61iIc) explains how to use the site to plan new running and biking courses.
+
+3.  **How Can I Use the Site While Marking or Running a Course?**
+
+   Trailmaster can be accessed from any Android or iOS device the same as you access it from your computer.  [This page](http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/) explains how to save Trailmaster to your mobile device's homescreen.
+
+   __Note:__ There are issues with WebGL on Galaxy S6 devices.  I am working on a workaround, but for now the map will not work on an S6.
+
+4.  **Is there an App Store Version?**
+
+   Not yet, but an Android app is in development.  Once finished it will have several additional capabilities the web site does not currently have.
+
+5.  **How Do I Share Trail Data?**
+
+   Contributing to the database of running and biking routes requires a GPX file exported from your GPS capable device.  See [this video](https://youtu.be/2LyszJVVjLk) for instructions on how to upload data.
+
+6.  **Why don't I see any Points of Interest or Routes?**
+
+   By default Trailmaster only displays data within 50 miles of your current location as reported by your web browser.  If you don't see any data, then no one has uploaded any data from that area yet.  Be the first!  Or change the distance filter settings using the settings link.
+
+7.  **I Found an Bug!**
+
+   Send me an email at [tjscollins@gmail.com](mailto:tjscollins@gmail.com) describing what you were doing and what happened.  Please include `Trailmaster Bug` in the subject line.  Alternatively, please go to the [issues page](https://github.com/tjscollins/trailmaster/issues) and create a new issue.
--- client/react/components/FAQ.jsx
+++ client/react/components/FAQ.jsx
@@ -1,8 +1,11 @@
 /*----------Modules----------*/
 import React from 'react';
+import Markdown from 'markdown-to-jsx';
 
 /*----------Components----------*/
 import BaseComponent from 'BaseComponent';
+
+let faqText = require('../../html/faq.md');
 
 export class FAQ extends BaseComponent {
   render() {
@@ -11,47 +14,18 @@ export class FAQ extends BaseComponent {
         <div className='modal-dialog' role='document'>
           <div className='modal-content'>
             <div className='modal-header'>
-              <button id='faq-modal-close' type='button' className='close' data-dismiss='modal' aria-label='Close'>
+              <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
                 <span aria-hidden='true'>&times;</span>
               </button>
               <h4 className='modal-title'>Frequently Asked Questions</h4>
             </div>
             <div className='modal-body'>
-              <ol>
-                <li>
-                  <h5>What is Trailmaster?</h5>
-                  <p>Trailmaster is a publicly accessible, open-source web application for storing,
-                    sharing, and using GPS data from wearable athletic devices to plan new trail
-                  running and mountain biking courses.</p>
-                </li>
-                <li>
-                  <h5>How Do I Use TrailMaster?</h5>
-                  <p>The website is meant to be simple to use. <a target='_blank' href='https://www.youtube.com/watch?v=spYawo61iIc'>This Youtube video</a> explains how to use the site to plan new running and biking courses.</p>
-                </li>
-                <li>
-                  <h5>How Can I Use the Site While Marking or Running a Course?</h5>
-                  <p>Trailmaster can be accessed from any Android or iOS device the same as you access it from your computer.  <a target='_blank' href='http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/'>This page</a> explains how to save Trailmaster to your mobile device's homescreen </p>
-                </li>
-                <li>
-                  <h5>Is There an App Store Version?</h5>
-                  <p>Not yet, but an Android version is in development.  Once finished it will have several new capabilities that the website does not have.</p>
-                </li>
-                <li>
-                  <h5>How Do I Contribute to the Database?</h5>
-                  <p>Contributing to the database of running and biking routes is simple once you know how to get a GPX file from your GPS-capable device.  See <a target='_blank' href='https://youtu.be/2LyszJVVjLk'>this video</a> for instructions on how to upload data.</p>
-                </li>
-                <li>
-                  <h5>Why Don't I See Any POIs or Routes?</h5>
-                  <p>Trailmaster is currently set to only display data within 50 miles of the center of your current map view.  If you don't see any data, then no one in that area has shared any data yet.  Be the first!  Or, change the distance filter settings using the settings dropdown menu.</p>
-                </li>
-                <li>
-                  <h5>I Found a Bug!</h5>
-                  <p>Send me an email at <a target='_blank' href='mailto:tjscollins@gmail.com'>tjscollins@gmail.com</a> describing what you were doing and what happened.  Please include 'Trailmaster Bug' in the subject line.  Alternatively, please go to the <a href='https://github.com/tjscollins/trailmaster/issues'>issues page</a> and create a new issue.</p>
-                  </li>
-              </ol>
+              <Markdown>
+                {faqText}
+              </Markdown>
             </div>
             <div className='modal-footer'>
-              <button type='submit' className='btn btn-default' data-dismiss='modal'>Dismiss</button>
+              <button id='faq-modal-close' type='submit' className='btn btn-default' data-dismiss='modal'>Dismiss</button>
             </div>
           </div>
         </div>
